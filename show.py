661ad32ab8d2528a71fc6859d6dc4d815e6bd2b2
zx013 <1453145267@qq.com>
2016-08-23 20:07:12 +0800

    step

--- show.py
+++ show.py
@@ -2,5 +2,27 @@
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
 
+#先放置地面，再放置其他的物品
 class Show(GridLayout):
-	pass
+	#移动到相邻的位置
+	def move(self):
+		pass
+
+	#原地踏步
+	def block(self):
+		pass
+
+	#怪物的动作
+	def monster(self):
+		pass
+
+	#战斗
+	def fight(self):
+		pass
+
+	#清除
+	def clear(self):
+		pass
+
+	def die(self):
+		pass
