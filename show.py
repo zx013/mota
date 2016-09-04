492dbf4a4e1868c2bc79e6f32e7dc34c56ac0c23
zx013 <1453145267@qq.com>
2016-09-04 16:50:57 +0800

    Trigger

--- show.py
+++ show.py
@@ -31,26 +31,51 @@ def logging(s):
 	Logger.warning(str(s))
 
 
-
 class Node(Image):
 	pass
 
 
-from functools import partial
-from itertools import cycle
+class Schedule:
+	from Queue import Queue
+
+	def __init__(self):
+		self.event = {}
+		Clock.schedule_interval(self.run, 0)
+
+	def run(self, t):
+		if self.event.has_key(Clock.frames):
+			while not self.event[Clock.frames].empty():
+				func, step, args, kwargs = self.event[Clock.frames].get()
+				func(step, *args, **kwargs)
+			del self.event[Clock.frames]
+
+	def trigger(self, func, iterator, interval, *args, **kwargs):
+		while True: #有可能下一帧会立刻执行，保证该函数执行完前，该帧不执行
+			frames = Clock.frames + 1
+			old_frames = frames
+			for step in iterator:
+				frames += interval
+				self.event.setdefault(frames, self.Queue())
+				self.event[frames].put((func, step, args, kwargs))
+			if old_frames == Clock.frames + 1:
+				break
+
+if 'Trigger' not in dir():
+	Trigger = Schedule().trigger
+
 
 class MoveHero(Image):
 	def __init__(self, **kwargs):
-		self.pos = kwargs['pos']
+		self.enable = True
+		self.pos = kwargs.get('pos', (0, 0))
 		self.size = (ShowBase.size, ShowBase.size)
 		super(MoveHero, self).__init__(**kwargs)
 		self.image = Image(source='data/action/hero/blue.png')
 		self.texture = self.image.texture.get_region(0, 1, ShowBase.size, ShowBase.size)
-		self.step = cycle(range(ShowBase.step)[::-1])
 
-	def next_step(self, key, moveable, t):
+	def next(self, step, key, moveable):
 		move = {'up': 0, 'down': 3, 'left': 2, 'right': 1}
-		self.texture = self.image.texture.get_region(self.step.next() * ShowBase.size, move[key] * (ShowBase.size + 1) + 1, ShowBase.size, ShowBase.size)
+		self.texture = self.image.texture.get_region(step * ShowBase.size, move[key] * (ShowBase.size + 1) + 1, ShowBase.size, ShowBase.size)
 		if moveable:
 				x, y = self.pos
 				if key == 'up':
@@ -64,8 +89,7 @@ class MoveHero(Image):
 				self.pos = x, y
 
 	def move(self, key, moveable=True):
-		for step in xrange(ShowBase.step):
-			Clock.schedule_once(partial(self.next_step, key, moveable), step * 0.05)
+		Trigger(self.next, range(ShowBase.step)[::-1], 5, key, moveable)
 
 
 #先放置地面，再放置其他的物品
