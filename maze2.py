29eb478584d09a5ab072540a302f120d5fc74bb6
zx013 <1453145267@qq.com>
2016-09-09 17:30:02 +0800

    step

--- maze2.py
+++ maze2.py
@@ -43,6 +43,15 @@ class MazeBase:
 		Type = 0
 		Value = 0
 
+	class NodeType:
+		none = 0
+		area_normal = 1
+		area_corner = 2
+		Area = (area_normal, area_corner)
+		road_normal = 3
+		road_corner = 4
+		Road = (road_normal, road_corner)
+
 
 class MazeSetting:
 	#层数
@@ -52,6 +61,17 @@ class MazeSetting:
 	#列
 	cols = 13
 
+
+class Pos:
+	@staticmethod
+	def beside(pos):
+		z, x, y = pos
+		return ((z, x - 1, y), (z, x + 1, y), (z, x, y - 1), (z, x, y + 1))
+
+	@staticmethod
+	def around(pos):
+		z, x, y = pos
+		return ((z, x - 1, y - 1), (z, x - 1, y), (z, x - 1, y + 1), (z, x, y - 1), (z, x, y + 1), (z, x + 1, y - 1), (z, x + 1, y), (z, x + 1, y + 1))
 
 class Maze2:
 	maze = []
@@ -76,22 +96,42 @@ class Maze2:
 		z, x, y = key
 		return self.maze[z][x][y]
 
-	def __iter__(self):
-		return ((k, i, j) for k in xrange(MazeSetting.floor) for i in xrange(MazeSetting.rows + 2) for j in xrange(MazeSetting.cols + 2))
-
 	def get_beside(self, pos, type):
-		z, x, y = pos
-		beside = ((z, x - 1, y), (z, x + 1, y), (z, x, y - 1), (z, x, y + 1))
-		return {(z, x, y) for z, x, y in beside if self.maze[z][x][y].Type == type}
+		return {(z, x, y) for z, x, y in Pos.beside(pos) if self.maze[z][x][y].Type == type}
 
 	def get_around(self, pos, type):
-		z, x, y = pos
-		around = ((z, x - 1, y - 1), (z, x - 1, y), (z, x - 1, y + 1), (z, x, y - 1), (z, x, y + 1), (z, x + 1, y - 1), (z, x + 1, y), (z, x + 1, y + 1))
-		return {(z, x, y) for z, x, y in around if self.maze[z][x][y].Type == type}
+		return {(z, x, y) for z, x, y in Pos.around(pos) if self.maze[z][x][y].Type == type}
 
 	#在floor层的type类型的区域中寻找符合func要求的点
 	def find_pos(self, floor, type, func):
 		return {(floor, i, j) for i in xrange(1, MazeSetting.rows + 1) for j in xrange(1, MazeSetting.cols + 1) if self[(floor, i, j)].Type == type and func((floor, i, j))}
+
+	#获取pos的类型area, road, end
+	def pos_type(self, pos):
+		beside_ground = self.get_beside(pos, MazeBase.Type.Static.ground)
+		if len(beside_ground) >= 3:
+			return MazeBase.NodeType.area_normal
+		if len(beside_ground) == 1:
+			return MazeBase.NodeType.road_corner
+		if len(beside_ground) == 2:
+			(z1, x1, y1), (z2, x2, y2) = beside_ground
+			if x1 == x2 or y1 == y2:
+				return MazeBase.NodeType.road_normal
+			if self[(z2, x2, y1) if pos == (z1, x1, y2) else (z1, x1, y2)].Type == MazeBase.Type.Static.wall:
+				return MazeBase.NodeType.road_normal
+			return MazeBase.NodeType.area_corner
+		return MazeBase.NodeType.none
+
+	def get_area(self, pos):
+		area = set()
+		beside = set([pos])
+		type = MazeBase.NodeType.Area if self.pos_type(pos) in MazeBase.NodeType.Area else MazeBase.NodeType.Road
+
+		while beside:
+			pos = beside.pop()
+			area.add(pos)
+			beside = beside | {beside_pos for beside_pos in self.get_beside(pos, MazeBase.Type.Static.ground) if self.pos_type(beside_pos) in type} - area
+		return area
 
 	def is_pure(self, pos):
 		if len(self.get_beside(pos, MazeBase.Type.Static.wall)) != 1:
@@ -104,62 +144,36 @@ class Maze2:
 	def get_pure(self, floor):
 		return self.find_pos(floor, MazeBase.Type.Static.ground, self.is_pure)
 
-	#给出一个被墙三面包围的点，获取该点延伸区域的所有点
-	def get_endarea(self, pos):
-		area = set()
-		if len(self.get_beside(pos, MazeBase.Type.Static.wall)) != 3:
-			return area
-		while True:
-			around = self.get_beside(pos, MazeBase.Type.Static.ground)
-			if len(around) > 2:
-				break
-			area.add(pos)
-			for around_pos in around:
-				if around_pos not in area:
-					pos = around_pos
-					break
-		return area
-
-	def add_wall(self, pos, num):
-		length = 0
+	def add_wall(self, pos):
 		while self.is_pure(pos):
 			self[pos].Type = MazeBase.Type.Static.wall
 			z, x, y = pos
 			z_wall, x_wall, y_wall = self.get_beside(pos, MazeBase.Type.Static.wall).pop()
 			pos = (z, 2 * x - x_wall, 2 * y - y_wall)
-			length += 1
-		if length == 0:
-			return
-		#如果长度为1的墙生成了大小为1的区域
-		if length == 1:
-			pos_set = set()
-			for pos in self.get_beside((z, x, y), MazeBase.Type.Static.ground):
-				if len(self.get_endarea(pos)) == 1:
-					pos_set.add(pos)
-			if pos_set:
-				return (z, x, y)
+
+	def clear_wall(self, floor):
+		func = lambda pos: self.pos_type(pos) == MazeBase.NodeType.road_corner and len(self.get_area(pos)) == 1
+		for pos in self.find_pos(floor, MazeBase.Type.Static.ground, func):
+			pass
 
 	def create(self):
 		#from show import logging
 		for floor in xrange(MazeSetting.floor):
-			clear_wall = set()
 			while True:
 				pure = self.get_pure(floor)
 				if not pure:
 					break
 				pos = random.choice(tuple(pure))
-				clear_wall.add(self.add_wall(pos, 1))
-			for pos in clear_wall:
-				if pos:
-					self[pos].Type = MazeBase.Type.Static.ground
+				self.add_wall(pos)
+			self.clear_wall(floor)
 
 	def show(self):
 		for i in xrange(MazeSetting.rows + 2):
 			for j in xrange(MazeSetting.cols + 2):
 				if self[(0, i, j)].Type == MazeBase.Type.Static.ground:
-					print ' ',
+					print self.pos_type((0, i, j)),
 				elif self[(0, i, j)].Type == MazeBase.Type.Static.wall:
-					print 1,
+					print  ' ',
 			print
 		print
 
